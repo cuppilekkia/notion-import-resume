ae994b44ab66fb05f72a5142865a1c3cfbdea78c
Antonio Coppola <coppola.ant@gmail.com>
2022-07-29 20:43:51 +0200

    better env var handling

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,8 +3,17 @@ dotenv.config()
 
 const SERVER_PORT = process.env.PORT || 8080
 const RESUME_PAGE_ID = process.env.RESUME_PAGE_ID || ""
+const NOTION_KEY = process.env.NOTION_KEY
+
+if (!NOTION_KEY) {
+    throw new Error("NOTION_KEY must be set")
+}
+if (!RESUME_PAGE_ID) {
+    throw new Error("RESUME_PAGE_ID must be set")
+}
 
 export {
     SERVER_PORT,
     RESUME_PAGE_ID,
+    NOTION_KEY
 }
